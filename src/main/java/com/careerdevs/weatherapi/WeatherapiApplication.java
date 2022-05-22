9bb6e052d0f1e03723b7259a5f18bd416ff555e2
961026bm <olubunmiosunde.student@careerdevs.com>
2022-05-22 15:55:22 -0400

    i

--- src/main/java/com/careerdevs/weatherapi/WeatherapiApplication.java
+++ src/main/java/com/careerdevs/weatherapi/WeatherapiApplication.java
@@ -1,5 +1,6 @@
 package com.careerdevs.weatherapi;
 
+import com.careerdevs.weatherapi.controllers.CurrentWeatherController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
